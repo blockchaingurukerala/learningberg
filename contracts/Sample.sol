67f194d2e41dce1e2fd9be5ee9d498a6a445b813
blockchaingurukerala <blockchaingurukerala@gmail.com>
2022-06-23 20:18:17 +0530

    second commint

--- contracts/Sample.sol
+++ contracts/Sample.sol
@@ -3,4 +3,9 @@ pragma solidity >=0.4.22 <0.8.0;
 contract Sample {
     string public name="Prince Kurian";
     string public str="Kottayam,Kerala,India";
+
+    function updatValue(string memory _n,string memory _a) public {
+        name=_n;
+        str=_a;
+    }
 }
